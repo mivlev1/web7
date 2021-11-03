eaa622213c08c7fe1fb7cd14859b0e6536349195
mivlev1 <makzoyn@gmail.com>
2021-11-03 20:50:57 +0300

    edit js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-$('.slider').slick({
-  infinite: true,
-  dots: true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
+$(document).ready(function () {
+  console.log('The DOM is now loaded and can be manipulated');
+  $('.slider').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+        }
       }
-    }
-  ]
+    ]
+  });
 });
